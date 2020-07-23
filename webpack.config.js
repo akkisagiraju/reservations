b127de362f78a8c309aa8d1ebbd5b7c82a2cbf57
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-07-23 18:03:08 +0530

    added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ const config = {
         exclude: /node_modules/
       },
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.(png|jp(e*)g|svg)$/,
         use: [
           {
